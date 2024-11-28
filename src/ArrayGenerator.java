6febdd8c21557246eb838e965bf2f0a65c4dba3d
SeungwonChoi <bestwondev@gmail.com>
2024-11-28 19:06:15 +0900

    Update ArrayGenerator.java

--- src/ArrayGenerator.java
+++ src/ArrayGenerator.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * 배열을 생성해주는 ArrayGenerator 클래스.
@@ -16,27 +17,41 @@ public class ArrayGenerator {
      * @param arrayType 배열의 타입. SORTED(오름차순 정렬), REVERSE(내람차순 정렬), RANDOM(각 원소를 무작위 배열) 세 가지 타입으로 생성 가능.
      * @return 지정된 타입으로 생성된 배열
      */
-    public Integer[] generateArray(int n, ArrayType arrayType) {
-        Integer[] array = new Integer[n];
+    public int[][] generateArray(int count, int n, ArrayType arrayType) {
+        int[][] arrays = new int[count][n];
 
         if (arrayType == ArrayType.SORTED) {
-            for (int i = 0; i < n; i++) {
-                array[i] = i;
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < n; j++) {
+                    arrays[i][j] = j;
+                }
             }
 
         } else if (arrayType == ArrayType.REVERSE) {
-            for (int i = 0; i < n; i++) {
-                array[i] = n - i - 1;
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < n; j++) {
+                    arrays[i][j] = n - j - 1;
+                }
             }
 
         } else if (arrayType == ArrayType.RANDOM) {
-            for (int i = 0; i < n; i++) {
-                array[i] = i;
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < n; j++) {
+                    arrays[i][j] = j;
+                }
+
+                // 배열을 리스트로 변환 후 셔플
+                List<Integer> tempList = Arrays.asList(Arrays.stream(arrays[i]).boxed().toArray(Integer[]::new));
+                Collections.shuffle(tempList);
+
+                // 리스트를 다시 배열로 변환
+                for (int j = 0; j < n; j++) {
+                    arrays[i][j] = tempList.get(j);
+                }
             }
 
-            Collections.shuffle(Arrays.asList(array));
         } else return null;
 
-        return array;
+        return arrays;
     }
 }

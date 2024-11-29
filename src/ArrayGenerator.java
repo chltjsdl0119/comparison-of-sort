18ae310b8b2abfd7119a85d76aeaf0e848242130
SeungwonChoi <bestwondev@gmail.com>
2024-11-29 18:02:11 +0900

    Update ArrayGenerator.java

--- src/ArrayGenerator.java
+++ src/ArrayGenerator.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 /**
  * 배열을 생성해주는 ArrayGenerator 클래스.
- * 인스턴스를 생성한 후, generateArray(int N, ArrayType arrayType) 메소드를 호출하여 배열을 생성한다.
+ * 인스턴스를 생성한 후, generateArray(int count, int n, ArrayType arrayType) 메소드를 호출하여 배열을 생성한다.
  */
 public class ArrayGenerator {
 
@@ -17,30 +17,30 @@ public class ArrayGenerator {
      * @param arrayType 배열의 타입. SORTED(오름차순 정렬), REVERSE(내람차순 정렬), RANDOM(각 원소를 무작위 배열) 세 가지 타입으로 생성 가능.
      * @return 지정된 타입으로 생성된 배열
      */
-    public int[][] generateArray(int count, int n, ArrayType arrayType) {
-        int[][] arrays = new int[count][n];
+    public int[][] generateArray(int testCount, int n, ArrayType arrayType) {
+        int[][] arrays = new int[testCount][n];
 
         if (arrayType == ArrayType.SORTED) {
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < testCount; i++) {
                 for (int j = 0; j < n; j++) {
                     arrays[i][j] = j;
                 }
             }
 
         } else if (arrayType == ArrayType.REVERSE) {
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < testCount; i++) {
                 for (int j = 0; j < n; j++) {
                     arrays[i][j] = n - j - 1;
                 }
             }
 
         } else if (arrayType == ArrayType.RANDOM) {
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < testCount; i++) {
                 for (int j = 0; j < n; j++) {
                     arrays[i][j] = j;
                 }
 
-                // 배열을 리스트로 변환 후 셔플
+                // 배열을 리스트로 변환 후 섞는다.
                 List<Integer> tempList = Arrays.asList(Arrays.stream(arrays[i]).boxed().toArray(Integer[]::new));
                 Collections.shuffle(tempList);
 
